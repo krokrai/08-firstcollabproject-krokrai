ac6fabbe7f439264ecbabd9087ed71724328cfa8
krokrai <mks3060@naver.com>
2026-05-03 15:39:45 +0900

    fix #16 : 이미지 누락 수정

--- Assets/03Script/Restaurant/CatEatMotion.cs
+++ Assets/03Script/Restaurant/CatEatMotion.cs
@@ -1,8 +1,14 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CatEatMotion : MonoBehaviour
 {
     [SerializeField] CatEatFish _cat;
+
+    private void Awake()
+    {
+        GetComponent<Image>().color = new Color(1, 1, 1, 0);
+    }
 
     public void MoreFish()
     {
